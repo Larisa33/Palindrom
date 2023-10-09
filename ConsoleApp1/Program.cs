df7a58a3fbcd5af2984de183f523023d6d2105dc
larisa33 <muntoiu.larisa@gmail.com>
2023-10-09 16:09:14 +0300

    palindrom

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introdu cuvantul: ");
+            string word = Console.ReadLine();
+
+            Console.WriteLine(IsPalindrom(word));
+
+        }
+
+        static string IsPalindrom(string word)
+        {
+
+            for (int i = 0; i < word.Length / 2; i++)
+            {
+                if (word[i] != word[word.Length - i - 1])
+                {
+                    return "Cuvantul introdus NU este un palindrom";
+                }
+            }
+
+            return "Cuvantul introdus este un palindrom";
+        
+        }
+    }
+}
